d74c3f4685c37f09bae3dc64f49ffbbfcd284911
day070 <hidayatsalmanan25@gmail.com>
2025-06-23 13:55:20 +0800

    menambahkan tanggal dan  hari

--- index.php
+++ index.php
@@ -1,19 +1,32 @@
  <!DOCTYPE html>
  <html lang="en">
+
  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>To Do List</title>
-    <link rel="stylesheet" href="style.css">
-    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>To Do List</title>
+   <link rel="stylesheet" href="style.css">
+   <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
  </head>
+
  <body>
-  <div class="container">
-    <div class="header">
-        <div class="content">
-            
-        </div>
-    </div>
-  </div>
+   <div class="container">
+     <div class="header">
+       <div class="title">
+         <i class='bx bx-sun'></i>
+         <span>To Do List</span>
+       </div>
+       <div class="description">
+         <?= date("l, d M Y") ?>
+       </div>
+     </div>
+   </div>
+   <div class="content">
+     <div class="card">
+
+     </div>
+   </div>
+   </div>
  </body>
+
  </html>
