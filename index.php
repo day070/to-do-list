b2df0fa52ee9f2d3a28a55c576470c2ff5470bf7
day070 <hidayatsalmanan25@gmail.com>
2025-06-24 15:20:13 +0800

    menambahkan table-task

--- index.php
+++ index.php
@@ -20,12 +20,43 @@
          <?= date("l, d M Y") ?>
        </div>
      </div>
-   </div>
-   <div class="content">
-     <div class="card">
+     <div class="content">
+       <div class="card">
+         <form action="" method="post">
+           <input type="text" class="input-control" name="" placeholder="Add task">
+           <div class="text-right">
+             <button type="submit" name="add">Add</button>
+           </div>
+         </form>
+       </div>
+
+       <div class="card">
+         <div class="task-items done">
+           <div>
+             <input type="checkbox" checked>
+             <span>Mandi</span>
+           </div>
+           <div>
+             <a href="" class="text-edit" title="Edit"><i class='bx bx-edit'></i></a>
+             <a href="" class="text-delete" title="Delete"><i class='bx bx-trash'></i></a>
+           </div>
+         </div>
+       </div>
+
+       <div class="card">
+         <div class="task-items">
+           <div>
+             <input type="checkbox">
+             <span>bangun tidur</span>
+           </div>
+           <div>
+             <a href="" class="text-edit" title="Edit"><i class='bx bx-edit'></i></a>
+             <a href="" class="text-delete" title="Delete"><i class='bx bx-trash'></i></a>
+           </div>
+         </div>
+       </div>
 
      </div>
-   </div>
    </div>
  </body>
 
