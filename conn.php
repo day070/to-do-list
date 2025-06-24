02419245335f6e698c87a9cc2a77549d5c73db8c
day070 <hidayatsalmanan25@gmail.com>
2025-06-24 19:09:36 +0800

    fix db

--- conn.php
+++ conn.php
@@ -1,7 +1,5 @@
-<?php 
-$conn = mysqli_connect("localhost","root","","todolist");
+<?php
+$conn = mysqli_connect("localhost", "root", "", "todolist");
 if (mysqli_connect_errno()) {
-    echo"error bung". mysqli_connect_error();
-}else{
-    echo "berhasil terhubung";
+    echo "error bung" . mysqli_connect_error();
 }
